ffd2033bacd0558000d703b78c336d19d08b71e2
Dominic Bosch <dominic.bosch@dobotech.ch>
2017-08-05 08:18:04 +0200

    functionality completed, now hardware tests

--- autohumi/autohumi.ino
+++ autohumi/autohumi.ino
@@ -3,7 +3,7 @@
 #define DHTTYPE DHT11
 
 // analog pins
-int aPotiPin = 0;
+int aPotiPin = 7;
 
 // digital pins
 int dDhtPin = 2;
@@ -13,11 +13,13 @@ int dInletPin = 9;
 int dOutletPin = 10;
 
 // Thresholds
+int minTemp = 15;
+int maxTemp = 35;
 int threshTemp = 25;
 int threshHumi = 40;
 
 boolean lastState = 0;
-int contrast = 0;
+unsigned char contrast = 0; // [0 - 255]
 int lcdbuf = 16;
 int state = 0; // init the run state
 int potiVal = 0;
@@ -31,14 +33,15 @@ void setup() {
   Serial.begin(9600);
   Serial.println("Initialising automatic humidifier");
   
+  // set pin states
   pinMode(dInletPin, OUTPUT);
   pinMode(dOutletPin, OUTPUT);
   pinMode(dLcdContrastPin, OUTPUT);
   pinMode(dSwitchPin, INPUT);
   
+  // contrast will be between 0 and 255
   // start the PWM signal with high contrast (low pwm value)
   analogWrite(dLcdContrastPin, contrast);
-      
   dht.begin();
   
   lcd.begin(lcdbuf, 2);
@@ -47,17 +50,11 @@ void setup() {
   lcd.print("Initialising...");
   
   delay(2000);
-  lcd.clear();
 }
 
 void loop() {
   time = millis();
   
-  // Normally we only check every ten seconds whether we need to start spraying water
-  if (abs(time - lastCheck) > 10000) {
-    checkAndSpray();
-    lastCheck = time;
-  }
   // Check whether the user whishes to switch the state
   boolean buttonPressed = debounceButton();
   if (buttonPressed != lastState) {
@@ -65,19 +62,20 @@ void loop() {
     if (buttonPressed) {
       state++;
       if (state > 3) state = 0;
-      
+
       lcd.clear();
       lcd.setCursor(0, 0);
       if (state == 0) {
-        lcd.print("Run state: ");
+        lcd.print("Humidity: ");
+        lcd.setCursor(0, 1);
+        lcd.print("Temp: ");
       } else if (state == 1) {
-      lcd.print("Set Temp threshold: ");
+        lcd.print("Temp. threshold:");
       } else if (state == 2) {
-      lcd.print("Set Humidity threshold: ");
+        lcd.print("Humi. threshold:");
       } else if (state == 3) {
-      lcd.print("Set Contrast threshold: ");
+        lcd.print("Set Contrast:");
       }
-      lcd.setCursor(12, 0);
   
       Serial.print("new state: ");
       Serial.println(state);
@@ -85,55 +83,43 @@ void loop() {
   }
   
   // if we are in one of the setting states, we read the poti value
-  if (state > 0) potiVal = readStablePotiValue();
-  switch(state) {
-    case 0: // run state
-      break;
-    case 1: // humidity setting state
+  if (state > 0) {
+    // potiVal will be [0 - 100]
+    Serial.print("\nRetrieving information from poti: ");
+    potiVal = readStablePotiValue();
+    Serial.print("Poti: ");
+    Serial.println(potiVal);
+    lcd.setCursor(0, 1);
+    switch(state) {
+      case 1: // humidity setting state
+        threshHumi = potiVal;
+        lcd.print(threshHumi);
+        break;
+      case 2: // temperature setting state
+        threshTemp = minTemp + (maxTemp - minTemp) / 100 * potiVal;
+        lcd.print(threshTemp);
+        break;
+      case 3: // contrast setting state
+        contrast = potiVal * 2.55;
+        lcd.print(contrast);
+        analogWrite(dLcdContrastPin, contrast);
+        break;
+    }
+  }
     
-      lcd.setCursor(12, 0);
-      lcd.print(potiVal);
-      break;
-    case 2: // temperature setting state
-      lcd.setCursor(12, 0);
-      lcd.print(potiVal);
-      break;
-    case 3: // contrast setting state
-      lcd.setCursor(12, 0);
-      lcd.print(potiVal);
-      
-      analogWrite(dLcdContrastPin, potiVal);
-      break;
+  // Normally we only check every ten seconds whether we need to start spraying water
+  // or if temperature and humidity are adjusted
+  if (state == 1 || state == 2 || abs(time - lastCheck) > 10000) {
+    checkAndSpray();
+    lastCheck = time;
   }
   
-
-//  delay(2000);
   // we need to check often for the pressed switch button
   delay(50);
 }
 
-boolean debounceButton() {
-  boolean state;
-  boolean previousState = digitalRead(dSwitchPin);
-  int i = 0;
-  // if we read 20 times the same value we are quite sure the button is stable
-  while (i < 20) {
-    delay(1);
-    state = digitalRead(dSwitchPin);
-    if (state != previousState) {
-      i = 0;
-      previousState = state;
-    }
-    i++;
-  }
-  return state;
-}
-
 void checkAndSpray() {
   
-  Serial.print("\nRetrieving information from sensor: ");
-  Serial.print("Poti: ");
-  Serial.println(potiVal);
 //  Serial.print("Read sensor: ");
   //delay(100);
   
@@ -145,41 +131,54 @@ void checkAndSpray() {
     return;
   }
   
+  if (state == 0) {
+    lcd.setCursor(12, 0);
+    lcd.print(humi);
+    lcd.setCursor(12, 1);
+    lcd.print(temp);
+  }
   float hic = dht.computeHeatIndex(temp, humi, false);
-  /*
-  lcd.setCursor(0, 0);
-  lcd.print("Humidity: ");
-  lcd.setCursor(12, 0);
-  lcd.print(humi);
-  */
+
   Serial.print("Humidity (%): ");
   Serial.println(humi);
-  /*
-  lcd.setCursor(0, 1);
-  lcd.print("Temp: ");
-  lcd.setCursor(12, 1);
-  lcd.print(temp);
-  */
+
   Serial.print("Temperature (°C): ");
   Serial.println(temp);
   
   Serial.print("Heat Index (oC): ");
   Serial.println(hic);
-  /*
-  if(temp > 26 || humi > 36) {
+  
+  if(temp > threshTemp || humi > threshHumi) {
     digitalWrite(dInletPin, HIGH);
   } else {
     digitalWrite(dInletPin, LOW);
-  }*/
+  }
+}
 
+boolean debounceButton() {
+  boolean state;
+  boolean previousState = digitalRead(dSwitchPin);
+  int i = 0;
+  // if we read 10 times the same value we are quite sure the button is stable
+  while (i++ < 10) {
+    delay(1);
+    state = digitalRead(dSwitchPin);
+    if (state != previousState) {
+      i = 0;
+      previousState = state;
+    }
+  }
+  return state;
 }
 
 // Returns a value between 0 and 100
 int readStablePotiValue() {
   int res = 0;
-  for(int i = 0; i < 10; i++) {
+  int num = 10;
+  for(int i = 0; i < num; i++) {
     res += analogRead(aPotiPin);
     delay(1);
   }
-  return res / 102.3; // poti values range from 0 to 1023
+  // poti values range from 0 to 1023 therefore the last 10 values correspond to 100
+  return res / num / 10.13;
 }
