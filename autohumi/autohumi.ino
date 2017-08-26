6a38dc3dca927dcc9c639e2289dc8196c1f0d041
Dominic Bosch <dominic.bosch@dobotech.ch>
2017-08-26 15:53:10 +0200

    OLED display and bugfixing

--- autohumi/autohumi.ino
+++ autohumi/autohumi.ino
@@ -1,9 +1,8 @@
 #include <LiquidCrystal.h>
 #include <DHT.h>
 #include <Arduino.h>
-#include <U8g2lib.h>
 #include <SPI.h>
-#include <Wire.h>
+#include <U8x8lib.h>
 
 #define DHTTYPE DHT11
 
@@ -28,21 +27,21 @@ boolean lastState = 0;
 // allowed values for contrast would be [0 - 255]
 // but since nothing can be seen above 75, we only use [0 - 75]
 unsigned char contrast = 75;
+int temperature = 0;
+int humidity = 0;
 int initialPotiVal;
 boolean potiChanged;
 int appState = 0; // init the app state
 int sprayState = 0; // init the spray state
 
-// OLED lines
-char line1[14] = "";
-char line2[14] = "";
-char line3[14] = "";
-char line4[14] = "";
-
+// OLED & LCD line buffer
+char oledLine[16] = "";
+char lcdLine[16] = "";
 
 // times:
 unsigned long nowTime = 0;
-unsigned long lastCheck = 0;
+unsigned long lastSprayCheck = 0;
+unsigned long lastTempCheck = 0;
 unsigned long sprayStart = 0;
 
 // Humidity & Temperatur sensor:
@@ -52,8 +51,7 @@ DHT dht(dDhtPin, DHTTYPE);
 LiquidCrystal lcd(11, 12, 5, 6, 7, 8);
 
 // OLED display:
-//U8G2_SSD1306_128X64_NONAME_1_SW_I2C u8g2(U8G2_R0, 19, 18);
-U8G2_SSD1306_128X64_NONAME_1_SW_I2C u8g2(U8G2_R2, 19, 18);
+U8X8_SSD1306_128X64_NONAME_SW_I2C u8x8(19, 18);
 
 void setup() {
   Serial.begin(9600);
@@ -70,7 +68,11 @@ void setup() {
   analogWrite(dLcdContrastPin, 75-contrast);
   dht.begin();
   
-  u8g2.begin();
+  u8x8.begin();
+  u8x8.setFlipMode(1);
+  u8x8.setFont(u8x8_font_pxplusibmcgathin_f);
+  u8x8.draw2x2String(0, 2, "Starting");
+  u8x8.draw2x2String(0, 4, "  up!");
   
   lcd.begin(16, 2);
   lcd.print("Auto Humidifier");
@@ -79,15 +81,41 @@ void setup() {
   
   delay(2000);
   
+  u8x8.clear();
   lcd.clear();
-  lcd.print("Humidity: ");
+  lcd.print("Humidity ");
   lcd.setCursor(0, 1);
-  lcd.print("Tmperature: ");
-  checkAndSpray();
+  lcd.print("Temperature ");
+  readTempAndHumi();
+  printDHTonLCD();
+  printDHTonOLED();
+  u8x8.drawString(0, 3, "Watching...");
 }
 
 void loop() {
   nowTime = millis();
+  boolean stateSwitched = updateState();
+  
+  // We check temperature and humidity every ten seconds
+  if (stateSwitched || abs(nowTime - lastTempCheck) > 10000) {
+    readTempAndHumi();
+    checkSpray();
+    printDHTonLCD();
+    printDHTonOLED();
+    lastTempCheck = nowTime;
+
+  // we check spray state every half second if we are spraying or flushing
+  }
+  else if ((sprayState == 1 || sprayState == 2) && abs(nowTime - lastSprayCheck) > 500) {
+    checkSpray();
+    lastSprayCheck = nowTime;
+  }
+
+  // we need to check often for the pressed switch button
+  delay(50);
+}
+
+boolean updateState() {
   boolean stateSwitched = false;
   // Check whether the user whishes to switch the state
   boolean buttonPressed = debounceButton();
@@ -107,30 +135,25 @@ void loop() {
   
   if (stateSwitched) {
     if (appState == 0) {
-      lcd.print("Humidity: ");
+      lcd.print("Humidity ");
       lcd.setCursor(0, 1);
-      lcd.print("Tmperature: ");
-      //sprintf(line3, "Monitoring...");
+      lcd.print("Temperature ");
     }
     else if (appState == 1) {
       lcd.print("Spray above:");
       printLCD(threshTemp, String((char)0xDF)+"C   ");
-      //sprintf(line3, "Set Temp.");
     }
     else if (appState == 2) {
       lcd.print("Spray below:");
       printLCD(threshHumi, "% humidity  ");
-      //sprintf(line3, "Set Humidity");
     }
     else if (appState == 3) {
-      lcd.print("Set Contrast:");
-      printLCD(contrast/0.75, "%   ");
-      //sprintf(line3, "Set Contrast");
-    }
-    else if (appState == 4) {
       lcd.print("Set spray time:");
       printLCD(sprayTime, " seconds   ");
-      //sprintf(line3, "Set Duration");
+    }
+    else if (appState == 4) {
+      lcd.print("Set Contrast:");
+      printLCD(contrast/0.75, "%   ");
     }
   }
   if (appState > 0) {
@@ -143,7 +166,7 @@ void loop() {
       initialPotiVal = potiVal;
       potiChanged = false;
     } else if (!potiChanged) {
-      if(abs(potiVal - initialPotiVal) > 10) {
+      if(abs(potiVal - initialPotiVal) > 5) {
         potiChanged = true;
       } else {
         potiVal = initialPotiVal;
@@ -155,19 +178,21 @@ void loop() {
         case 1: // temperature setting state
           threshTemp = minTemp + (maxTemp - minTemp) * potiVal / 100;
           printLCD(threshTemp, String((char)0xDF)+"C   ");
+          //printDHTonOLED();
           break;
         case 2: // humidity setting state
           threshHumi = potiVal;
           printLCD(threshHumi, "% humidity  ");
+          //printDHTonOLED();
           break;
-        case 3: // contrast setting state
+        case 3: // Spray Duration Setting [60 - 600] seconds
+          sprayTime = 30 + potiVal * 5.7;
+          printLCD(sprayTime, " seconds   ");
+          break;
+        case 4: // contrast setting state
           contrast = potiVal * 0.75; // [0 - 75] are reasonable values
           printLCD(potiVal, "%   ");
           analogWrite(dLcdContrastPin, 75 - contrast);
-          break;
-        case 4: // Spray Duration Setting [60 - 600] seconds
-          sprayTime = 60 + potiVal * 5.4;
-          printLCD(sprayTime, " seconds   ");
           break;
         // case 4: // brightness setting state
         //   brightness = potiVal * XYZ; // what are reasonable values?
@@ -177,77 +202,77 @@ void loop() {
       }
     }
   }
-  
-  // #
-  // # FIXME: we need to separate check for spraying from flushing and stopping
-  // # FIXME: spraying every 10s and the others every 0.5s
-  // #
-  
-  // Normally we only check every ten seconds whether we need to start spraying water
-  // or if temperature and humidity are adjusted
-  if (stateSwitched || appState == 1 || appState == 2 || abs(nowTime - lastCheck) > 10000) {
-    checkAndSpray();
-    lastCheck = nowTime;
-  }
-  
-  // we need to check often for the pressed switch button
-  delay(50);
-  drawOLED();
+  return stateSwitched;
 }
 
-void printLCD(int val, String unit) {
-  lcd.setCursor(0, 1);
-  lcd.print(val);
-  lcd.print(unit);
-}
-
-void checkAndSpray() {
+void readTempAndHumi() {
   int humi = dht.readHumidity();
   int temp = dht.readTemperature();
+
   if (isnan(humi) || isnan(temp)) {
     Serial.println("Failed to read from DHT sensor!");
     return;
   }
+  humidity = humi;
+  temperature = temp;
+}
 
-  sprintf(line1, "%3d C > %2d C", temp, threshTemp);
-  line1[3] = (char)176;
-  line1[10] = (char)176;
-  sprintf(line2, "%3d%%  <%3d%%", humi, threshHumi);
-  
-  if (appState == 0) {
-    lcd.setCursor(12, 0);
-    lcd.print(humi);
-    lcd.print("%  ");
-    lcd.setCursor(12, 1);
-    lcd.print(temp);
-    lcd.print((char)0xDF);
-    lcd.print("C  ");
-  }
-  
+void checkSpray() {
   // if we are idle and the thresholds are crossed, we start spraying 
-  if (sprayState == 0 && temp > threshTemp || humi < threshHumi) {
+  if (sprayState == 0 && (temperature > threshTemp || humidity < threshHumi)) {
     digitalWrite(dInletPin, HIGH);
     sprayStart = nowTime;
     sprayState = 1;
     Serial.println("New spray state: Spraying!");
-    sprintf(line3, "Spraying!");
-  
+    u8x8.drawString(0, 3, "Spraying!  ");
+    
   // if we are spraying and the time is over, we stop spraying and flush
   } else if (sprayState == 1 && abs(nowTime - sprayStart)/1000 > sprayTime) {
     digitalWrite(dInletPin, LOW);
     digitalWrite(dOutletPin, HIGH);
     sprayState = 2;
     Serial.println("New spray state: Flushing!");
-    sprintf(line3, "Flushing!");
+    u8x8.drawString(0, 3, "Flushing!  ");
 
   // we flush the pipeline for 30 seconds
   } else if (sprayState == 2 && abs(nowTime - sprayStart)/1000 > (sprayTime+30)) {
     digitalWrite(dOutletPin, LOW);
     sprayState = 0;
     Serial.println("New spray state: Watching!");
-    sprintf(line3, " Watching...");
-    sprintf(line4, "");
+    u8x8.drawString(0, 3, "Watching...");
+    u8x8.drawString(0, 4, "");
   }
+  
+  if (sprayState == 1) {
+    sprintf(oledLine, "Ends in %3ds", sprayStart/1000 + sprayTime - nowTime/1000);
+    u8x8.drawString(0, 4, oledLine);
+  } else if (sprayState == 2) {
+    sprintf(oledLine, "Ends in %3ds", sprayStart/1000 + (sprayTime+30) - nowTime/1000);
+    u8x8.drawString(0, 4, oledLine);
+  }
+}
+
+void printDHTonLCD() {
+  if (appState == 0) {
+    lcd.setCursor(12, 0);
+    sprintf(lcdLine, "%3d%%  ", humidity);
+    lcd.print(lcdLine);
+    
+    lcd.setCursor(12, 1);
+    sprintf(lcdLine, "%2d C  ", temperature);
+    lcdLine[2] = (char)0xDF;
+    lcd.print(lcdLine);
+/*    lcd.print(temperature);
+    lcd.print((char)0xDF);
+    lcd.print("C  ");*/
+  }
+}
+
+void printDHTonOLED() {
+  sprintf(oledLine, "%3d°C now > %2d°C", temperature, threshTemp);
+  u8x8.drawUTF8(0, 0, oledLine);
+  sprintf(oledLine, "%3d%%  now <%3d%%", humidity, threshHumi);
+  u8x8.drawString(0, 1, oledLine);
 }
 
 // Since the switches vibrate physically when used, we have to wait for an accurate value
@@ -281,21 +306,13 @@ int readStablePotiValue() {
   return 100 - (res / num / 10.13); // we flip the value because of the poti direction (100 - ...)
 }
 
-
-void drawOLED() {
-  if (sprayState == 1) {
-    sprintf(line4, "Ends in %3ds", sprayStart/1000 + sprayTime - nowTime/1000);
-  } else if (sprayState == 2) {
-    sprintf(line4, "Ends in %3ds", sprayStart/1000 + (sprayTime+30) - nowTime/1000);
-  }
-
-  u8g2.firstPage();
-  do {
-    u8g2.setFont(u8g2_font_9x15_tf);
-    u8g2.drawStr(0, 10, line1);
-    u8g2.drawStr(0, 25, line2);
-    u8g2.drawStr(0, 49, line3);
-    u8g2.drawStr(0, 64, line4);
-  } while ( u8g2.nextPage() );
+void printLCD(int val, String unit) {
+  lcd.setCursor(0, 1);
+  lcd.print(val);
+  lcd.print(unit);
 }
-
+/*
+void drawOLED(int line, char * str) {
+  u8x8.drawString(0, line, str);
+}
+*/
